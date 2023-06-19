04d48748cfd1d4e89b6d332b5ff51e9572b4b784
110000110 <lll29604@gmail.com>
2023-06-20 06:35:16 +0800

    view card

--- screens/view_savedCard.js
+++ screens/view_savedCard.js
@@ -1,58 +1,40 @@
-import { StyleSheet, Image, Text, View, Button, TouchableOpacity, ImageBackground } from 'react-native'
-import React ,{useState, useEffect, useCallback} from 'react'
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { StyleSheet, View, Image } from 'react-native'
+import React from 'react'
 import {ShopCard} from '../components/shopCard'
-import {drawcardStyles} from '../components/drawcard_style'
-import {globalStyles} from '../components/global_style'
-import  {setGlobalState, useGlobalState} from '../shared/states'
-import {GetallCards, SaveCard, ClearallCards, readFirstCard} from '../shared/myCard_data'
 
+export default function View_savedCard({ route }) {
+  const {card, shown} = route.params;
 
-export default function View_savedCard({ route, navigation }) {
-  
-  const {card} = route.params;
-
-  const loadImage = async (photo_reference) => {
-    try {
-      const res = await fetch(
-        `${CORS_ANYWHERE_HOST}${places_photoUrl}${photo_reference}&key=${apiKey}`
-      );
-      const data = await res.blob();
-      setDrawncard(drawnCard=> {
-        return{
-          ...drawnCard,
-          photoUrl: URL.createObjectURL(data)
-        }
-      })
-
-      
-    } catch (error) {
-      console.error(`error in photo api: ${error}`);
-    }
-  }
-
-
-  console.log('in view save card');
   return (
     <View style={styles.container}>
-      <View style={{position: 'relative',  top: 120, alignSelf: 'center'}} >
-
-        <ShopCard 
-            drawnCard = {card} 
-            phtoUrl = {card.photoUrl}
-             />
+      <View style={styles.content}>
+        <ShopCard drawnCard={card}/>
+        {
+          (shown) ?
+            <Image style={styles.conversationImg} source={require(`../assets/conversation/section-burgerman-speech-drawFromMyCard-result.png`)}/>
+          :
+            null
+        }
       </View>
-
-      
     </View>
   )
-  
 }
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     backgroundColor: '#D7F8F0',
-    height: '100%'
-   
+    flex: 1
+  },
+  content: {
+    position: 'relative',
+    top: 120,
+    alignSelf: 'center'
+  },
+  conversationImg: {
+    height: 70,
+    width: 300,
+    alignSelf: 'center',
+    marginBottom: 10,
+    marginTop: 15
   }
-})
+});
